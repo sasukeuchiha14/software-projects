a6d9530598d3b2ebc58eb666ffae1ec19ce381f8
sasukeuchiha14 <hgarg7123@gmail.com>
2024-03-07 08:42:05 +0530

    Removed functions from case_study

--- C/case_study.c
+++ C/case_study.c
@@ -4,20 +4,6 @@
 
 #define MAX_ROOMS 10
 
-struct Customer {
-    char name[50];
-    int age;
-    int number;
-    char address[100];
-};
-
-void displayCustomerInfo(struct Customer customer) {
-    printf("\nName: %s\n", customer.name);
-    printf("Age: %d\n", customer.age);
-    printf("Phone Number: %d\n", customer.number);
-    printf("Address: %s\n", customer.address);
-}
-
 typedef struct {
     int roomNumber;
     bool isReserved;
@@ -25,96 +11,93 @@ typedef struct {
 
 Room rooms[MAX_ROOMS];
 
-void initializeRooms() {
+int main() {
+    int choice;
+    char yn[5];
+
     for (int i = 0; i < MAX_ROOMS; i++) {
         rooms[i].roomNumber = i + 1;
         rooms[i].isReserved = false;
     }
-}
 
-void displayRooms() {
-    printf("Available Rooms:\n");
-    for (int i = 0; i < MAX_ROOMS; i++) {
-        if (!rooms[i].isReserved) {
-            printf("Room %d\n", rooms[i].roomNumber);
-        }
-    }
-}
+    char name[50];
+    int age;
+    int number;
+    char address[100];
 
-bool reserveRoom(int roomNumber) {
-    if (roomNumber < 1 || roomNumber > MAX_ROOMS) {
-        printf("Invalid room number!\n");
-        return false;
-    }
-
-    else if (rooms[roomNumber - 1].isReserved) {
-        printf("Room %d is already reserved!\n", roomNumber);
-        return false;
-    }
-
-    else {
-        rooms[roomNumber - 1].isReserved = true;
-        printf("Room %d reserved successfully!\n", roomNumber);
-        return true;
-    }
-}
-
-bool cancelReservation(int roomNumber) {
-    if (roomNumber < 1 || roomNumber > MAX_ROOMS) {
-        printf("Invalid room number!\n");
-        return false;
-    }
-
-    else if (!rooms[roomNumber - 1].isReserved) {
-        printf("Room %d is not reserved!\n", roomNumber);
-        return false;
-    }
-
-    else {
-        rooms[roomNumber - 1].isReserved = false;
-        printf("Reservation for room %d cancelled successfully!\n", roomNumber);
-        return true;
-    }
-}
-
-int main() {
-    int choice;
-    struct Customer customer;
-    char yn[5];
-    initializeRooms();
     int roomNumber;
     while (true) {
+
         printf("\nMenu:\n1. Customer info\n2. Display available rooms\n3. Reserve a room\n4. Cancel Reservation\n5. Exit\n\nEnter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
                 printf("Enter customer name: ");
-                scanf("%s", customer.name);
+                scanf("%s", name);
                 printf("Enter customer age: ");
-                scanf("%d", &customer.age);
+                scanf("%d", &age);
                 printf("Enter customer phone number: ");
-                scanf("%d", &customer.number);
+                scanf("%d", &number);
                 printf("Enter customer address: ");
-                scanf("%s", customer.address);
-                displayCustomerInfo(customer);
+                scanf("%s", address);
+                printf("\nName: %s\n", name);
+                printf("Age: %d\n", age);
+                printf("Phone Number: %d\n", number);
+                printf("Address: %s\n", address);
                 break;
+
             case 2:
-                displayRooms();
+                printf("Available Rooms:\n");
+                for (int i = 0; i < MAX_ROOMS; i++) {
+                    if (!rooms[i].isReserved) {
+                        printf("Room %d\n", rooms[i].roomNumber);
+                    }
+                }
                 break;
+
             case 3:
                 printf("Enter the room number to reserve: ");
                 scanf("%d", &roomNumber);
-                reserveRoom(roomNumber);
+                {
+                    if (roomNumber < 1 || roomNumber > MAX_ROOMS) {
+                        printf("Invalid room number!\n");
+                    }
+
+                    else if (rooms[roomNumber - 1].isReserved) {
+                        printf("Room %d is already reserved!\n", roomNumber);
+                    }
+
+                    else {
+                        rooms[roomNumber - 1].isReserved = true;
+                        printf("Room %d reserved successfully!\n", roomNumber);
+                    }
+                }
                 break;
+
             case 4:
                 printf("Enter the room number to cancel reservation: ");
                 scanf("%d", &roomNumber);
-                cancelReservation(roomNumber);
+                {
+                    if (roomNumber < 1 || roomNumber > MAX_ROOMS) {
+                        printf("Invalid room number!\n");
+                    }
+
+                    else if (!rooms[roomNumber - 1].isReserved) {
+                        printf("Room %d is not reserved!\n", roomNumber);
+                    }
+
+                    else {
+                        rooms[roomNumber - 1].isReserved = false;
+                        printf("Reservation for room %d cancelled successfully!\n", roomNumber);
+                    }
+                }
                 break;
+
             case 5:
                 printf("Exiting...\n");
                 return 0;
+
             default:
                 printf("Invalid choice! Please try again.\n");
                 break;
