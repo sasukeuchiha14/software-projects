b99e67bc6347266645d42fa543d8b8ba8a3667be
sasukeuchiha14 <hgarg7123@gmail.com>
2024-04-23 16:38:11 +0530

    Updated QrMaker.py

--- Python/Qr Maker.py
+++ Python/Qr Maker.py
@@ -1,9 +1,8 @@
 import qrcode
 import pyautogui as pag
 
-
 link = pag.prompt(text="", title="Enter the Link of which you want Qr")
 img = qrcode.make(link)
-type(img)  # qrcode.image.pil.PilImage
-file_format = pag.prompt(text="", title="Enter the File Name.Format")
+type(img)
+file_format = pag.prompt(text=" *.jpg / *.jpeg /*.png ", title="Enter the File Name.Format")
 img.save(file_format)
