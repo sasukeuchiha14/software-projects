3b70d8a42b0de29287b3b74682be1fc811b9d162
sasukeuchiha14 <hgarg7123@gmail.com>
2024-04-23 16:33:58 +0530

    Fixed afk.py

--- Python/afk.py
+++ Python/afk.py
@@ -2,17 +2,17 @@ import pyautogui as pag
 import random
 import time
 
-c=int(input("For How many Hours you want : "))
-a=c*3600
-if c==0:
-    b=int(input("For How many Minutes you want : "))
-a=b*60
-if b==0:
-    a=int(input("For How many Minutes you want : "))
+b=int(input("For How many Hours you want : "))
+a=b*3600
+c=int(input("For How many Minutes you want : "))
+a+=c*60
+d=int(input("For How many Seconds you want : "))
+a+=d
+
 i=0
-while i<a:
-    x=random.randint(600,700)
-    y=random.randint(200,600)
+while i<(a-2):
+    x=random.randint(1,1919)
+    y=random.randint(1,1079)
     pag.moveTo(x,y,0.5)
     time.sleep(2)
     i=i+2
