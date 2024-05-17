43f87cecbf0146604f648acaf1c37d9a8ef4308e
sasukeuchiha14 <hgarg7123@gmail.com>
2024-05-17 19:26:50 +0530

    Updated QR Maker - added option to save.

--- Python/Qr Maker.py
+++ Python/Qr Maker.py
@@ -1,8 +1,22 @@
 import qrcode
 import pyautogui as pag
+import winsound
 
 link = pag.prompt(text="", title="Enter the Link of which you want Qr")
+
 img = qrcode.make(link)
-type(img)
+img.show()
+
 file_format = pag.prompt(text=" *.jpg / *.jpeg /*.png ", title="Enter the File Name.Format")
-img.save(file_format)
+
+try:
+    file_name,format = file_format.split(".")
+except:
+    print("Invalid Format")
+else:
+    if format == "jpg" or format == "jpeg" or format == "png":
+        img.save(file_format)
+    else:
+        print("Invalid Format")
+finally:
+    winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
