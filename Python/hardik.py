7b624345f35708be0e05562fa60f3dcef23067c0
sasukeuchiha14 <hgarg7123@gmail.com>
2024-07-02 22:22:26 +0530

    Removes some unappropriate files

--- Python/hardik.py
+++ Python/hardik.py
@@ -8,33 +8,12 @@ import pyautogui as pag
 asakey = Controller()
 asakey2 = C2()
 
-class sex:
-    
-    def open_incognito():
-        subprocess.call("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
-
-        time.sleep(1)
-
-        asakey.press(Key.ctrl);time.sleep(0.2);asakey.press(Key.shift);time.sleep(0.2);asakey.press('n')
-        time.sleep(0.2)
-        asakey.release(Key.ctrl);time.sleep(0.2);asakey.release(Key.shift);time.sleep(0.2);asakey.release('n')
-    
-    def search():
-        sentence = pag.prompt(text="", title="Enter the Search Prompt")
-        pag.moveTo(500,80,0.5)
-        for i in sentence:
-            asakey.press(i)
-            asakey.release(i)
-            time.sleep(0.1)
-        asakey.press(Key.enter)
-        asakey.release(Key.enter)
-
 class install():
     
     def pip_install():
         
         Popen("cmd", creationflags=CREATE_NEW_CONSOLE)
-        input = pag.prompt(text="", title="Enter the thing which you wanna install Prompt")
+        input = pag.prompt(text="", title="Enter the thing which you wanna install (FULL Prompt)")
         
         try:
             sentence = input.lower()
