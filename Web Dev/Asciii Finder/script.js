658614d6a898e54a004e98a98d7e1cdd3fe42121
sasukeuchiha14 <hgarg7123@gmail.com>
2024-10-07 22:30:26 +0530

    Make ascii finder for phone also. (conveted div with text to input tag)

--- Web Dev/Asciii Finder/script.js
+++ Web Dev/Asciii Finder/script.js
@@ -1,3 +1,29 @@
-window.addEventListener('keydown', function(e) {
-    this.document.getElementsByClassName('container')[0].innerHTML = `${e.key} : ${e.key.charCodeAt(0)}`;
-});
+const inputField = document.getElementById('inputField')
+
+function detectDevice() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    
+    if (/mobile|android|iphone|ipad|tablet/.test(userAgent)) {
+        console.log("You're using a mobile or tablet.");
+        
+        inputField.addEventListener('input', function(e) {
+            setInterval(() => {}, 100);
+            const currentValue = inputField.value[inputField.value.length - 1];
+            inputField.placeholder = `${currentValue} : ${currentValue.charCodeAt(0)}`;
+            inputField.value = '';
+        });
+        
+        inputField.focus();
+    }
+    else {
+        console.log("You're using a laptop or PC.");
+        
+        window.addEventListener('keydown', function(e) {
+            inputField.placeholder = `${e.key} : ${e.key.charCodeAt(0)}`;
+            // this.document.getElementsByTagName('input')[0].value = '';
+        });
+    }
+}
+
+// Run on page load
+window.onload = detectDevice;
